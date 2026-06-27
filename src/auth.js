a5fca88983e6a54ed97c343963f769c8ef70fc6e
petev <plamen.g.petev@gmail.com>
2026-06-27 15:52:35 +0300

    TASK-003: Add user validation

--- src/auth.js
+++ src/auth.js
@@ -2,3 +2,8 @@
 function login() {
     return true;
 }
+
+// Step 1 - Validate user credentials
+function validateUser(username, password) {
+    return username.length > 0 && password.length > 0;
+}
